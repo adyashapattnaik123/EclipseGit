2417385936a3a6a58053bf314e19cbd4de1ceb73
adyashapattnaik123 <pattnaikadyasha99@gmail.com>
2020-09-14 12:27:54 +0530

    Conflict Resolved

--- ExploringJava/src/HelloWorld.java
+++ ExploringJava/src/HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.Date;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		System.out.println("Hello World 1");
 		Date d=new Date();
 		System.out.println(d);
 		LocalDateTime dt=LocalDateTime.now();
